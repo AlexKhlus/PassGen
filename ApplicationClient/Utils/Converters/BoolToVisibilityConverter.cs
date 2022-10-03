4dac1dc2146f0b0ad3783888a15cda0ec37b8aaa
Alexander Radchenko <radchenkoalexd@gmail.com>
2022-10-03 18:28:53 +0300

    Added converters

---
+++ ApplicationClient/Utils/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+
+namespace ApplicationClient.Utils.Converters;
+public sealed class BoolToVisibilityConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		=> targetType == typeof(Visibility) && (bool)value ? Visibility.Collapsed : Visibility.Visible;
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		=> targetType == typeof(bool) && value is Visibility.Visible;
+}
---
+++ ApplicationClient/Utils/Converters/MultipleToSingleParameterConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace ApplicationClient.Utils.Converters;
+
+[ValueConversion(typeof(object), typeof(object))]
+public class MultipleToSingleParameterConverter : IMultiValueConverter
+{
+	public object Convert(
+		object[] values, 
+		Type targetType, 
+		object parameter, 
+		CultureInfo culture
+	) 
+	{
+		if(targetType != typeof(object)) throw new InvalidOperationException("The target must be a object");
+		return values.Clone();
+	}
+
+	public object[] ConvertBack(
+		object value,
+		Type[] targetTypes, 
+		object parameter, 
+		CultureInfo culture
+	) => throw new NotSupportedException();
+}
