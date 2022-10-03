b9a944322551d68523b6cf58eef11315052c67b8
Alexander Radchenko <radchenkoalexd@gmail.com>
2022-10-03 18:31:42 +0300

    Added stores

---
+++ ApplicationClient/Utils/Stores/NavigationStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ApplicationClient.ViewModels.Base;
+
+
+namespace ApplicationClient.Utils.Stores;
+public sealed class NavigationStore 
+{
+	private ViewModel _currentViewModel;
+
+	public ViewModel CurrentViewModel 
+	{
+		get => _currentViewModel;
+		set
+		{
+			var prevViewModel = _currentViewModel;
+
+			_currentViewModel?.Dispose();
+			_currentViewModel = value;
+
+			CurrentViewModelChanged?.Invoke(sender: this, e: new () {
+				PrevViewModel = prevViewModel,
+				CurrViewModel = _currentViewModel
+			});
+		}
+	}
+
+	public event EventHandler<CurrentViewModelChangedEventArgs>? CurrentViewModelChanged;
+}
+
+public class CurrentViewModelChangedEventArgs : EventArgs 
+{
+	internal ViewModel? PrevViewModel { get; init; }
+	internal ViewModel? CurrViewModel { get; init; }
+}
---
+++ ApplicationClient/Utils/Stores/UserStore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ApplicationAPI.Data.Models;
+
+
+namespace ApplicationClient.Utils.Stores;
+public sealed class UserStore
+{
+	private User _currentUser;
+
+	public event EventHandler? CurrentViewModelChanged;
+
+	public User CurrentUser 
+	{
+		get => _currentUser;
+		set
+		{
+			_currentUser = value;
+			OnCurrentUserChanged();
+		}
+	}
+
+	private void OnCurrentUserChanged() 
+	{
+		CurrentViewModelChanged?.Invoke(this, EventArgs.Empty);
+	}
+}
