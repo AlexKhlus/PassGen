d050793b6b969dbf633f04aa062f99b1b3d7eccc
Alexander Radchenko <radchenkoalexd@gmail.com>
2022-10-03 18:30:48 +0300

    Added base class for view models

---
+++ ApplicationClient/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+
+namespace ApplicationClient.ViewModels.Base;
+public abstract class ViewModel : INotifyPropertyChanged, IDisposable 
+{
+	[NotifyPropertyChangedInvocator] 
+	protected void InvokePropertyChanged([CallerMemberName] string propertyName = "")
+		=> PropertyChanged?.Invoke(sender: this, e: new (propertyName));
+
+	public virtual void Dispose() 
+	{
+		// override
+	}
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+}
