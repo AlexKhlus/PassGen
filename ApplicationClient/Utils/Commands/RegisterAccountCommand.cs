74b97b32539b929a9a3b263b0c365b72dd82e8a4
Alexander Radchenko <radchenkoalexd@gmail.com>
2022-11-15 23:55:43 +0200

    Changed registration logic, removed email field

---
+++ ApplicationClient/Utils/Commands/RegisterAccountCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using ApplicationAPI.Resolvers;
+using ApplicationClient.Utils.Commands.Base;
+
+
+namespace ApplicationClient.Utils.Commands;
+public sealed class RegisterAccountCommand : AsyncCommand
+{
+	private readonly IAccountResolver _accountResolver;
+	private readonly NavigateToLoginViewCommand _navigateToLoginViewCommand;
+
+	public RegisterAccountCommand(
+		IAccountResolver accountResolver, 
+		NavigateToLoginViewCommand navigateToLoginViewCommand
+	) 
+	{
+		_accountResolver = accountResolver;
+		_navigateToLoginViewCommand = navigateToLoginViewCommand;
+	}
+
+	public override async Task ExecuteAsync(object? parameter = null) 
+	{
+		var parameters = (object[])parameter!;
+		var (
+			username,
+			password, 
+			repeatPassword
+		) = (
+			parameters[0].ToString()!,
+			parameters[1].ToString()!,
+			parameters[2].ToString()!
+		);
+
+		var response = await _accountResolver.RegisterAccount(new () {
+			Username = username,
+			Password = password,
+			RepeatPassword = repeatPassword
+		});
+		if(response.Success)
+		{
+			await Task.Delay(1000);
+			await _navigateToLoginViewCommand.ExecuteAsync();
+		}
+		else MessageBox.Show($"Wrong credentials!{Environment.NewLine}{response.ErrorMessage}");
+	}
+}
---
+++ ApplicationClient/Views/ResetAccountView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+
+namespace ApplicationClient.Views;
+public partial class ResetAccountView : UserControl
+{
+	public ResetAccountView()
+	{
+		InitializeComponent();
+	}
+}
