9b1209df6e97b9b4e720206b7f3cef6fee4f97dc
Alexander Radchenko <radchenkoalexd@gmail.com>
2022-11-15 23:52:07 +0200

    Added dynamic database creation

--- ApplicationAPI/Data/Database/DbConnectionFactory.cs
+++ ApplicationAPI/Data/Database/DbConnectionFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using ApplicationAPI.Data.Database.Base;
+using Dapper;
 using Microsoft.Data.Sqlite;
 
 
@@ -10,5 +11,21 @@ public sealed class DbConnectionFactory : IDbConnectionFactory
 
 	public DbConnectionFactory(string connectionString) => _connectionString = connectionString;
 
-	public IDbConnection CreateConnection() => new SqliteConnection(_connectionString);
+	public IDbConnection CreateConnection()
+	{
+		var connection = new SqliteConnection(_connectionString);
+		connection.Execute(@"
+create table if not exists User
+(
+    Id integer not null
+        constraint UserId_pk
+            primary key autoincrement,
+    Username TEXT not null
+        constraint UserUsername_pk
+            unique,
+    Password TEXT not null
+);");
+
+		return connection;
+	}
 }
